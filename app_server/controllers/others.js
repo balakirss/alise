8b02be99612843ee604087b5f0ddd00b40540151
balakirss <sse2010@yandex.ru>
2019-06-09 18:03:48 +0300

    доделал

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -2,5 +2,5 @@
 // render - функция express компиляции шаблона страницы
 // index - имя шаблона index.jade
 module.exports.about = function(req, res) {
-    res.render('index', {title: 'About'});
+    res.render('generic-text', {title: 'About'});
 };
