d1efb4f4729f1cc06cdc23f941589041f290c51f
balakirss <sse2010@yandex.ru>
2019-06-17 01:11:09 +0300

    fff

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -31,15 +31,16 @@ module.exports.homelist = function(req, res) {
             maxDistance : 99999
         }
     };
+
     request(
         requestOptions,
         function (err, response, body) {
             var i, data;
             data = body;
-            for (i=0; i <data.length; i++){
+            for (i = 0; i < data.length; i++) {
                 data[i].distance = _formatDistance(data[i].distance);
             }
-            renderHomepage(req,res, data);
+            renderHomepage(req, res, data);
         }
     );
     };
