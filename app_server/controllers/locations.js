e20f348fb3c6c58ab9268ac29da6d3b5e2abb72f
balakirss <sse2010@yandex.ru>
2019-06-22 12:18:46 +0300

    ищю ошибку

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -37,8 +37,10 @@ module.exports.homelist = function(req, res) {
         function (err, response, body) {
             var i, data;
             data = body;
-            for (i = 0; i < data.length; i++) {
-                data[i].distance = _formatDistance(data[i].distance);
+            if (response.statusCode === 200 && data.length) {
+                for (i = 0; i < data.length; i++) {
+                    data[i].distance = _formatDistance(data[i].distance);
+                }
             }
             renderHomepage(req, res, data);
         }
